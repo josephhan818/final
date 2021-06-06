7e9ee96c0b08fe73e1bd6b7ccc882a6ba68df3bb
josephhan818 <79505369+josephhan818@users.noreply.github.com>
2021-06-06 21:42:57 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,4 +13,10 @@
         pagination: {
           el: ".swiper-pagination",
         },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+       
+
       });
